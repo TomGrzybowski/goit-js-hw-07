6c3c161c02f4c4fe9e96190cc1cfad6a3fdd35f3
TomekGrzybek <t.grzybowski94@gmail.com>
2022-12-17 14:45:53 +0100

    zad 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
 console.log(galleryItems);
+
+//Create galery by inserting images and links
+galleryItems.forEach((el) => {
+  const link = document.createElement("a");
+  link.classList.add("gallery__item");
+  link.href = el.original;
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = el.preview;
+  img.alt = el.description;
+
+  link.insertAdjacentElement("beforeend", img);
+  gallery.insertAdjacentElement("beforeend", link);
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
